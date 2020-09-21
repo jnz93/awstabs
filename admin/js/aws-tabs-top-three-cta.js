f4921013079d9ec11045c4a7fbc7486ab16359b5
joanezandrades <box@unitycode.tech>
2020-09-21 11:08:01 -0300

    Top 3 + cta link

--- admin/js/aws-tabs-top-three-cta.js
+++ admin/js/aws-tabs-top-three-cta.js
@@ -11,11 +11,11 @@
                 'format': 'html'
             });
 
-            var titleDefault = '<h3>Adicione um título para tabela</h3>',
-                openTable = '<table><tbody>',
-                rowOne = '<tr><th style="text-align: left;">1º - </th><td style="text-align: left;">' + selected_text + '</td><td style="text-align: left;"><button class="btn-primary">CTA 1</button></td></tr>',
-                rowTwo = '<tr><th style="text-align: left;">2º - </th><td style="text-align: left;">' + selected_text + '</td><td style="text-align: left;"><button class="btn-primary">CTA 2</button></td></tr>',
-                rowThree = '<tr><th style="text-align: left;">3º - </th><td style="text-align: left;">' + selected_text + '</td><td style="text-align: left;"><button class="btn-primary">CTA 3</button></td></tr>',
+            var titleDefault = '<h3>Top 3 Produto X</h3>',
+                openTable = '<table class="simple" style="border: none"><tbody>',
+                rowOne = '<tr><td style="text-align: left; width: 32%;">1º - </td><td style="text-align: left; width: 50%;">' + selected_text + '</td><td style="text-align: left; width: 18%;"><button class="btn-primary">CTA 1</button></td></tr>',
+                rowTwo = '<tr><td style="text-align: left; width: 32%;">2º - </td><td style="text-align: left; width: 50%;">' + selected_text + '</td><td style="text-align: left; width: 18%;"><button class="btn-primary">CTA 2</button></td></tr>',
+                rowThree = '<tr><td style="text-align: left; width: 32%;">3º - </td><td style="text-align: left; width: 50%;">' + selected_text + '</td><td style="text-align: left; width: 18%;"><button class="btn-primary">CTA 3</button></td></tr>',
                 closeTable = '</tbody></table>',
                 returnText = '';
 
